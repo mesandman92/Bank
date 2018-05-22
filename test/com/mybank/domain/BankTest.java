25f501c681331602e16d0860cb3806c6a281ba5e
mesandman92 <mesandman2@yandex.by>
2018-05-22 22:59:46 +0300

    Added JUnit Bank test class

---
+++ test/com/mybank/domain/BankTest.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mybank.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author uladzimir
+ */
+public class BankTest {
+
+    Bank bank = Bank.getBank();
+
+    Customer firstCustomer = new Customer("John Doe");
+    Customer secondCustomer = new Customer("Jane Doe");
+
+    public BankTest() {
+
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        SavingsAccount johnSavings = new SavingsAccount(1000, 5);
+        CheckingAccount johnAccount = new CheckingAccount(5000, 1000);
+        CheckingAccount janeAccount = new CheckingAccount(500, 100);
+        firstCustomer.addAccount(johnSavings);
+        firstCustomer.addAccount(johnAccount);
+        secondCustomer.addAccount(janeAccount);
+        bank.addCustomer(firstCustomer);
+        bank.addCustomer(secondCustomer);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getBank method, of class Bank.
+     */
+    @Test
+    public void testGetBank() {
+        System.out.println("getBank - only one instance of Bank");
+        Bank expResult = bank;
+        Bank result = Bank.getBank();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCustomer method, of class Bank.
+     */
+    @Test
+    public void testGetCustomer() {
+        System.out.println("getCustomer");
+        int customerNumber = 0;
+        Bank instance = bank;
+        Customer expResult = firstCustomer;
+        Customer result = instance.getCustomer(customerNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of addCustomer method, of class Bank.
+     */
+    @Test
+    public void testAddCustomer() {
+        System.out.println("addCustomer");
+        Customer newCustomer = new Customer("John Cena");
+        Bank instance = bank;
+        instance.addCustomer(newCustomer);
+        Customer expResult = newCustomer;
+        Customer result = instance.getCustomer(2);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getNumOfClients method, of class Bank.
+     */
+    @Test
+    public void testGetNumOfClients() {
+        System.out.println("getNumOfClients");
+        Bank instance = bank;
+        int expResult = 0;
+        int result = instance.getNumOfClients();
+        assertEquals(expResult, result);
+    }
+
+}
